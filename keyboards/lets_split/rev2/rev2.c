5bd8f7f6b712c2489ff15d7d0501972b36788f02
Soenke Hack <hack.soenke@gmail.com>
2017-11-18 20:36:27 +0100

    turn off pro micro leds

--- keyboards/lets_split/rev2/rev2.c
+++ keyboards/lets_split/rev2/rev2.c
@@ -19,13 +19,13 @@ void matrix_init_kb(void) {
         PLAY_SONG(tone_startup);
     #endif
 
-    // // green led on
-    // DDRD |= (1<<5);
-    // PORTD &= ~(1<<5);
+    // green led on
+    DDRD &= ~(1<<5);
+    PORTD &= ~(1<<5);
 
-    // // orange led on
-    // DDRB |= (1<<0);
-    // PORTB &= ~(1<<0);
+    // orange led on
+    DDRB &= ~(1<<0);
+    PORTB &= ~(1<<0);
 
 	matrix_init_user();
 };
